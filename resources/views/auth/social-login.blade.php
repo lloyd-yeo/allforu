2967ca14639d06ac6ae398575fb8c3f6025d0204
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-05-04 05:31:33 +0800

    Web: Changed facebook button bigger

--- resources/views/auth/social-login.blade.php
+++ resources/views/auth/social-login.blade.php
@@ -179,7 +179,7 @@
                                 @elsedesktop
                                     <div>
                                         <div class="fb-login-button" data-max-rows="1"
-                                             data-size="small" data-button-type="login_with"
+                                             data-size="medium" data-button-type="login_with"
                                              data-show-faces="false"
                                              data-auto-logout-link="false"
                                              data-use-continue-as="false"></div>
